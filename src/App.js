cddbc8deeef56b67ea9f4b7e8d6ec58f826cf521
Gerard Albajar <gerard.albajar@gmail.com>
2022-04-26 20:26:35 +0200

    data alphabet uploaded and imported at app component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import Hangman from "./components/Hangman/Hangman";
 import Letters from "./components/Letters/Letters";
 import Result from "./components/Result/Result";
 import UsedLetters from "./components/UsedLetters/UsedLetters";
+import alphabet from "./data/alphabet";
 
 function App() {
-  // const selectMyLetter = () => {
-
-  // };
-
   return (
     <>
       <div className="container">
