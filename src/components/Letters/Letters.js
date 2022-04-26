f5f2c568a604350c88175b1f26c292d8dc10d6d2
Gerard Albajar <gerard.albajar@gmail.com>
2022-04-26 19:22:17 +0200

    usedletters component updated

--- src/components/Letters/Letters.js
+++ src/components/Letters/Letters.js
@@ -27,61 +27,61 @@ const Letters = ({ action }) => {
       <li className="letter" onClick={action}>
         <a href="h">H</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="i">I</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="j">J</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="k">K</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="l">L</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="m">M</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="n">N</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="ñ">Ñ</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="o">O</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="p">P</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="q">Q</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="r">R</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="s">S</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="t">T</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="u">U</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="v">V</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="w">W</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="x">X</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="y">Y</a>
       </li>
-      <li className="letter">
+      <li className="letter" onClick={action}>
         <a href="z">Z</a>
       </li>
     </ul>
